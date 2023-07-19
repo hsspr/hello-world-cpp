54e342ae80cd2efecbbdc7971e55f1c240cda023
hsspr <59533059+hsspr@users.noreply.github.com>
2023-07-19 14:34:34 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   int a = 0;
-  std::cout << "Hello World from main branch on codespaces !" << std::endl;
+  std::cout << "Hello World from main branch on codespaces edited by test account !" << std::endl;
   a = 1;
   return 0;
 }
